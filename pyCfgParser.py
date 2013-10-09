47dc9836ea37d71e8a2a01a8dd807c6589ad4c12
Dongpu Jin <jindongpu@gmail.com>
2013-10-09 11:52:20 -0500

    Count pref types.

--- pyCfgParser.py
+++ pyCfgParser.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python
 
+# Dongpu Jin
+# 10/01/2013
+
 import xml.parsers.expat; 
 import os; 
 
 propCnt = 0;
 propCntGlobal = 0; 
-numOfFiles = 0; 
-currPath = ""; 
-allPrefsList = []; 
-prefFileList = []; 
+currPath = "";  # The current path to the pref
+allPrefsList = [];  # A list of (distinct) prefs
+prefFileList = [];  # A list of pref files
+isValue = 0;  # Whether a value field
+valueCnt = 0;  # Number of values
+isNewPref = 0;   # Whether a new pref
+boolTypeCnt = 0;  # Number of boolean types 
+intTypeCnt = 0;  # Number of integer types 
+otherTypeCnt = 0;  # Number of other types 
 
 # handler functions 
 def start_element(name, attrs): 
@@ -16,27 +24,60 @@ def start_element(name, attrs):
     global propCntGlobal; 
     global currPath; 
     global allPrefsList; 
+    global isValue; 
+    global isNewPref; 
     
     if (name == "prop"): 
         propCnt = propCnt + 1; 
         propCntGlobal = propCntGlobal + 1; 
-        allPrefsList.append(os.path.join(currPath, attrs["oor:name"])); 
-        
+        prefPath = os.path.join(currPath, attrs["oor:name"]); 
+        if (prefPath not in allPrefsList):  
+            isNewPref = 1;  # A new pref
+            allPrefsList.append(prefPath); 
+        else: 
+            isNewPref = 0; 
+            
     if (name == "item"): 
         currPath = attrs["oor:path"];  # Start a new path 
 
+    if (name == "value"): 
+        isValue = 1; 
+        
 def end_element(name):
     global currPath; 
+    global isValue; 
+    global isNewPref; 
+    
     if (name == "item"): 
+        isNewPref = 0;   # Reset new pref flag
         currPath = "";   # Close the current path
 
+    if (name == "value"): 
+        isValue = 0; 
+        
 def char_data(data): 
-    pass; 
-    #print 'Character data:', repr(data); 
+    global isValue; 
+    global valueCnt; 
+    global boolTypeCnt; 
+    global intTypeCnt;
+    global otherTypeCnt;
+     
+    if (isValue and isNewPref): 
+        valueCnt = valueCnt + 1; 
+        # print type(data);  
+        # print type(data.encode("ascii", "ignore")); 
+        dataStr = data.encode("ascii", "ignore"); 
+        if (dataStr == "true" or dataStr == "false"):  # boolean type
+            boolTypeCnt = boolTypeCnt + 1; 
+        else:         
+            try: 
+                int(dataStr);   # If succeed, must be integer type 
+                intTypeCnt = intTypeCnt + 1; 
+            except Exception:   # If failed, must be other types 
+                otherTypeCnt = otherTypeCnt + 1;  
     
 def process_curr_directory(directoryPath): 
-    global propCnt;
-    global numOfFiles; 
+    global propCnt; 
     global prefFileList; 
     
     for root, subDirectories, files in os.walk(directoryPath): 
@@ -50,8 +91,7 @@ def process_curr_directory(directoryPath):
                 
                 filePath = os.path.join(root, filename); 
                 prefFileList.append(filePath);  
-                
-                numOfFiles = numOfFiles + 1;  
+                 
                 with open(filePath, "r") as f:
                     propCnt = 0; 
                     p.Parse(f.read());    
@@ -65,5 +105,8 @@ process_curr_directory(os.curdir);
 print "allPrefsList Length:", len(allPrefsList); 
 print "allPrefsList Set Length: ", len(set(allPrefsList)); 
 print "propCnt: ", propCntGlobal;  
-print "numOfFiles: ", numOfFiles; 
 print "prefFileList Length: ", len(prefFileList); 
+print "value count: ", valueCnt; 
+print "boolTypeCnt: ", boolTypeCnt; 
+print "intTypeCnt: ", intTypeCnt; 
+print "otherTypeCnt: ", otherTypeCnt; 
